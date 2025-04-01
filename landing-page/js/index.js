de5743ba5535bc2f31e6f366071387665c9dc3cd
andreyjodar <andreyviniciusjodar@gmail.com>
2025-03-31 21:31:31 -0300

    Alterações JavaScript e CSS

--- landing-page/js/index.js
+++ landing-page/js/index.js
@@ -1,26 +1,48 @@
 document.addEventListener("DOMContentLoaded", (event)=>{
     buscarInscritos();
+    construirModal();
 });
 
 function alterarTema() {
     //DOM -> document object model
-    const theme = document.body.getAttribute("data-theme");
-    const newTheme = theme == 'dark' ? 'light' : 'dark';
-    document.body.setAttribute("data-theme", newTheme);
+    const tema = document.body.getAttribute("data-theme");
+    const novoTema = tema == 'dark' ? 'light' : 'dark';
+    /* let novoTema = '';
+     if(tema == 'dark'){
+         novoTema = 'light';
+     }else{
+         novoTema = 'dark';
+     }*/
+    document.body.setAttribute("data-theme", novoTema);
 
-    const btAlterarTema = document.getElementById("btAlterarTema")
-    btAlterarTema.textContent = theme == 'dark' ? 'Dark' : 'Light';
+    const btAlterarTema = document.getElementById("btAlterarTema");
+    btAlterarTema.textContent = novoTema == 'dark' ? 'Light' : 'Dark';
 }
 
-function buscarInscritos(){
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res => res.json())
-    .then(res => {
-        const divInscritos = document.getElementById('inscritos');
-        res.forEach(user => {
-            const novoParagrafo = document.createElement("p");
-            novoParagrafo.textContent = `Name: ${user.name}`;
-            divInscritos.appendChild(novoParagrafo);
-        })
-    }).catch(e => console.log(e));
+function buscarInscritos() {
+    fetch("json/inscritos.json")
+        .then(res => res.json())
+        .then(res => {
+            const divInscritos = document.getElementById('inscritos');
+            res.forEach(user => {
+                const novoParagrafo = document.createElement("p");
+                novoParagrafo.textContent = `Nome: ${user.nome}`;
+                divInscritos.appendChild(novoParagrafo);
+            });
+        }).catch(e => console.log(e));
+}
+
+function construirModal(){
+    const botaoSaibaMais = document.getElementById("saiba-mais");
+    const modal = document.getElementById("modal");
+    botaoSaibaMais.addEventListener("click", ()=>{
+        modal.classList.remove("hidden")
+    });
+
+    window.addEventListener("click", (e)=>{
+        if(e.target == modal){
+            modal.classList.add("hidden");
+        }
+
+    })
 }
